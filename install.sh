0cad8787c791366bc6cdd496b4651a6a6243a6f8
mohaxars <34405744+mohaxars@users.noreply.github.com>
2018-02-04 12:49:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+################################################################################
+################################################################################
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nano git -y
+sudo apt-get install build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev miniupnpc libminiupnpc-dev autoconf pkg-config libtool autotools-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev automake -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get install upstart-sysv -y
